fd0831657e352b5c707a5df8ed02740d2b4c77dc
elqumsan <m.qumsan@gmail.com>
2024-05-15 22:18:19 +0000

    further update

---
+++ Functions/norm_scale_dim_cluster_qc.R
@@ -0,0 +1,25 @@
+
+
+
+
+what_dims <- function(object_type , path,  .){
+  
+  object_type <-AddModuleScore(object = object_type , features = genes, ctrl = 100, name = 'ribo_Features' )
+  
+  return(object_type)
+}
+
+
+# QC: Violin plot of nfeatures_RNA, percentmt, for ecah cluster
+
+QC_plot <- function(data ){
+  
+  p <- ggplot(data, aes_string(x= "seurat_clusters" , y=120 , colors= "seurat_clusters")) + 
+    geom_violin()+
+    geom_boxplot(width= 0.07, outlier.shape = NA, color = "black", alpha= 0.7) +
+    theme_bw()+
+    theme(legend.position = "none", axis.title.x =  element_blank())
+  return(p)
+  
+}
+  
